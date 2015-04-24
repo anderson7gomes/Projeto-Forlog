3590f20bde1c666a28866a2dd566511e15fcc9d1
CaioViktor <caioviktor@hotmail.com>
2015-04-24 16:50:06 -0300

    Mudança Atomo Implementação Formula e No(Árvore Sintática)

--- Atomo.java
+++ Atomo.java
@@ -1,15 +1,39 @@
 public class Atomo{
 	private boolean valor;
-	public Atomo(boolean valor){
+	private char rotulo;
+	public Atomo(char rotulo,boolean valor){
 		this.valor = valor;
+		this.rotulo = rotulo;
+	}
+	public Atomo(char rotulo ){
+		this.rotulo = rotulo;
+		valor = false;
 	}
 	public Atomo(){
-		this(false);
+		this('');
 	}
 	public boolean getValor(){
 		return valor;
 	}
 	public void setValor(boolean valor){
 		this.valor = valor;
+	}
+	public char getRotulo(){
+		return rotulo;
+	}
+	public void setRotulo(char rotulo){
+		this.rotulo = rotulo;
+	}
+	public boolean equals(Object e){
+		if(e == this)
+			return true;
+		if(!e instanceof Atomo)
+			return false;
+		if(e.getRotulo() == this.getRotulo())
+			return true;
+		return false;
+	}
+	public String toString(){
+		return "Rotulo: " + rotulo + "\tValor: " + valor;
 	}
 }
---
+++ Formula.java
@@ -0,0 +1,25 @@
+public class Formula{
+	private No arvore;
+	private boolean valor;
+	private int qtdAtomos;
+	private Atomo atomos[];
+
+	public Formula(String formula){
+		//TODO: ler a string e formar a arvore
+		valor = false; // TODO:calcular
+		qtdAtomos = 0; // TODO:calcular
+		atomos = new Atomo[qtdAtomos];
+	}
+	public boolean getValor(){
+		return valor;
+	}
+	public int quantidadeAtomos(){
+		return qtdAtomos;
+	}
+	public boolean getValorProposicao(int indice){
+		if(indice > quantidadeAtomos())
+			throw new IllegalArgumentException("Posição não existente");
+		return atomos[indice];
+	}
+	//TODO: setValorPosicao e etc
+}
