bac6a5c5dbaedded00f5d8bcfc4bb9bbd8166374
Anderson Gomes <gomes.andrade1@gmail.com>
2015-04-29 16:00:58 -0300

    Incluindo métodos verificadores na classe Conectivo

--- Conectivo.java
+++ Conectivo.java
@@ -13,5 +13,38 @@ public abstract class Conectivo {
     public String getTipo() {
         return tipo.toString();
     }
+    
+    public static boolean isConectivoValido(char character) {
+    
+    	if ((character == '\u2227') || (character == '\u2228') || 
+    			(character == '\u2192') || (character == '\u2194') || 
+    			(character == '\u223c')) {
+    		return true;		
+    	}		
+    	
+    	return false;
+    
+    }
+    
+    public static boolean isConectivoBinarioValido(char character) {
+    
+    	if ((character == '\u2227') || (character == '\u2228') || 
+    			(character == '\u2192') || (character == '\u2194')) {
+    		return true;		
+    	}
+    	
+    	return false;		
+    
+    }
+    
+    public static boolean isConectivoUnarioValido(char character) {
+    
+    	if (character == '\u223c') {
+    		return true;
+    	}
+    	
+    	return false;
+    
+    }
 
 } // fim da classe Conectivo
