b1f721754a6c375cb1f81f052c674e8a7e4e18aa
Caio Viktor Avila <caioviktor@hotmail.com>
2015-06-17 10:13:28 -0300

    Comentários na JPanelD

--- source/JPanelD.java
+++ source/JPanelD.java
@@ -23,13 +23,13 @@ public class JPanelD extends JPanel {
 		
 	}
 	
-	private int setUpTree(No no, int limX, int y, int dist) {
+	private int setUpTree(No no, int limX, int y, int dist) { //Descobre o posicionamento dos nós
 	
-		y += tamanho;
+		y += tamanho;//abaixa o nó pelo tamanho de seu pai
 		
 		if (no.isFolha()) {
 		
-			int x = limX + dist + tamanho;
+			int x = limX + dist + tamanho;//sua posição horizontal recebe o limite mínimo de x somada a distância mínima e seu tamanho
 			
 			no.setX(x);
 			no.setY(y);
@@ -38,30 +38,30 @@ public class JPanelD extends JPanel {
 
 		}
 		
-		int xEs = setUpTree(no.getFilhoEsquerdo(),limX ,y + dist,dist);
+		int xEs = setUpTree(no.getFilhoEsquerdo(),limX ,y + dist,dist);//Caucula a posição de seu filho esquerdo
 		int x = xEs;
-		int lim = xEs;
+		int lim = xEs;//impõe que todos os nós que serão calculados devarão vir depois de seu filho esquerdo
 		
 		if (no.getFilhoDireito() != null) {
 		
-			int xDi = setUpTree(no.getFilhoDireito(), xEs, y + dist, dist);
+			int xDi = setUpTree(no.getFilhoDireito(), xEs, y + dist, dist);//Caucula a posição de seu filho direito
 
-			x = (int) ((xEs + xDi) / 2);
-			lim = xDi;
+			x = (int) ((xEs + xDi) / 2);//Coordenada X do pai recebe a média das dos filhos
+			lim = xDi;//impõe que todos os nós que serão calculados devarão vir depois de seu filho direito
 
 		}
 		
 		no.setX(x);
 		no.setY(y);
 		
-		return lim;
+		return lim;//Retorna o limite na horizontal para os próximos nós
 
 	}
 	
 	public void paint(Graphics g) {
 
-		g.clearRect(0, 0, lim, lim);
-        drawTree(raiz, g, 30);
+		g.clearRect(0, 0, lim, lim);//Limpa área de desenho
+        drawTree(raiz, g, 30);//redesenha
 
 	}
 	
@@ -76,7 +76,7 @@ public class JPanelD extends JPanel {
 		String texto;
 		Object ob = no.getElemento();
 		
-		if (ob instanceof Atomo) {
+		if (ob instanceof Atomo) {//Consegue os tótulos dos nós
 		    
 			Atomo ob1 = (Atomo) ob;
 			texto = String.valueOf(ob1.getRotulo());
@@ -85,32 +85,32 @@ public class JPanelD extends JPanel {
 			texto = String.valueOf(ob);
 		}
 		
-		g.drawString(texto, (x + (tamanho / 2)), (y + (tamanho / 2)));
+		g.drawString(texto, (x + (tamanho / 2)), (y + (tamanho / 2))); //Desenha o rótulo no centro do nó
 
 
 		if (!no.isFolha()) {
 		
-			if (no.getFilhoDireito() != null) {
+			if (no.getFilhoDireito() != null) {//Verifica se é binário ou unário
 			
 				int xEs = no.getFilhoEsquerdo().getX();
 				int xDi = no.getFilhoDireito().getX();
 				int yEs = no.getFilhoEsquerdo().getY();
 				int yDi = no.getFilhoDireito().getY();
 				
-				g.drawLine(x + (tamanho) / 2, y + tamanho, xEs + tamanho / 2, yEs);
-				drawTree(no.getFilhoEsquerdo(), g,dist);
+				g.drawLine(x + (tamanho) / 2, y + tamanho, xEs + tamanho / 2, yEs);//Desenha linha do pai até o filho esquerdo
+				drawTree(no.getFilhoEsquerdo(), g,dist);//Desenha filho esquerdo
 
 
-				g.drawLine(x + (tamanho) / 2, y + tamanho, xDi + tamanho / 2, yDi);
-				drawTree(no.getFilhoDireito(), g, dist);
+				g.drawLine(x + (tamanho) / 2, y + tamanho, xDi + tamanho / 2, yDi);//Desenha linha do pai até o filho direito
+				drawTree(no.getFilhoDireito(), g, dist);//Desenha filho direito
 				
-			} else {
+			} else {//Operação unária
 			
 				g.drawLine(x + (tamanho) / 2, y + tamanho, 
 						no.getFilhoEsquerdo().getX() + tamanho / 2, 
-				        no.getFilhoEsquerdo().getY());
+				        no.getFilhoEsquerdo().getY());//Desenha uma linha reta
 
-				drawTree(no.getFilhoEsquerdo(), g, dist);
+				drawTree(no.getFilhoEsquerdo(), g, dist);//Desenha filho
 
 			}
 		}
